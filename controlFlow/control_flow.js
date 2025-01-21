64a7da7e4cb199f55bea034db14379cf90fd420f
manuthRJ <manuthrajarathna@gmail.com>
2025-01-21 01:45:31 -0500

    Control Flow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,54 @@
+//User Role
+
+let userRole = "admin";
+let accessLevel;
+
+if (userRole==="admin"){
+    accessLevel = "Full access granted";
+} else if (userRole === "manager"){
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level: ",accessLevel);
+
+//User LogedIn
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn){
+    if (userRole === "admin"){
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, user!";
+    }
+} else {
+    userMessage = "Please Log in to access the system";
+}
+
+console.log("User Message: ",userMessage);
+
+//Switch Case
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType){
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    
+    case "manager":
+        userCategory = "Manager";
+        break;
+
+    case "subscriber":
+        userCategory = "Subscriber"
+        break;
+
+    default:
+        userCategory = "Unkonown";
+}
+
+console.log("UserCategory: ", userCategory)
